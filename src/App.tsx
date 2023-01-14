203f1d8579559e6e59fe7bd10ae1f94a6136ce5d
varshin-sid <varshinsiddhapura@gmail.com>
2023-01-15 00:12:23 +0530

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Wrapper></Wrapper>;
 }
 
 export default App;
+
+// Create a Wrapper component that'll render a <section> tag with some styles
+const Wrapper = styled.div`
+  display: flex;
+  background: rgb(240, 242, 245);
+  height: 100vh;
+`;
